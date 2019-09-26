91a54a1dff30773ae123baf7d7d855caf6717b08
antuniooh <51217271+antuniooh@users.noreply.github.com>
2019-09-26 18:06:53 -0300

    Add files via upload

--- Grafos.py
+++ Grafos.py
@@ -1,21 +1,19 @@
-matriz=[] 
-num = 0 
-m = []
-
 def grafoSimples(matriz):
+    l = 0
+    am = 0
     for linha in range(len(matriz)): 
         for coluna in range(len(matriz[linha])):
-            if(linha == coluna and matriz[linha][coluna] == 1):
+            if(linha == coluna and matriz[linha][coluna] == 2):
                 print("Há laço no vertice %s" %(linha+1))
-            else:
-                pass
+                l = 1
+            if (linha != coluna and matriz[linha][coluna] > 1):
+                print("Há aresta multiplas nos vertices %s %s" % ((linha + 1), (coluna + 1)))
+                am = 1
 
-    for linha in range(len(matriz)): 
-        for coluna in range(len(matriz[linha])):
-            if(matriz[linha][coluna] > 1):
-                print("Há aresta multiplas nos vertices %s %s" %((linha+1), (coluna+1)))
-            else:
-                pass
+    if l == 0:
+        print("Não há laço no grafo")
+    if am == 0:
+        print("Não há arestas multiplas no grafo")
 
 def Sequencia(matriz):
     sequencia= []
@@ -26,21 +24,20 @@ def Sequencia(matriz):
         sequencia.append(grau)
         grau=0
     sequencia.sort(reverse=True)
+    print("Sequencia de graus do grafo: %s" %sequencia)
     return sequencia
-    
 
 def Arestas(S):
     T=0
     for coluna in range(len(S)):
         T += S[coluna]
-    return int(T/2)
+    #a quantidade de arestas é a soma dos graus dos vertices sobre dois
+    print("A quantidade de arestas do grafo é : %d" %(T/2))
 
 def Completo(S):
     T=0
-    for coluna in range(len(S)):
-        if(S[coluna] == (len(S) - 1)):
-            T += 1
-            
+    T = S.count(len(S)-1)
+
     if (T == len(S)):
         print("A matriz é completa, pois todos os vertices incidem sobre todos os outros")
     else:
@@ -61,25 +58,28 @@ def Printar(matriz):
         for coluna in range(len(matriz[linha])): 
             print("%2d" % matriz[linha][coluna], end=" ")
         print()
-    
 
-arquivo = open("garfos.txt","r")
-matriz = []
-for linha in arquivo.readlines():
-    resultado = linha.strip().split()
-    matriz.append(resultado)
-for linha in range(len(matriz)): 
-    for coluna in range(len(matriz[linha])): 
-         matriz[linha][coluna] = int(matriz[linha][coluna])
+def bipartido(matriz):
+    m=0
 
-    
-Printar(matriz)
-grafoSimples(matriz)
-S = Sequencia(matriz)
-print("Sequencia de graus do grafo: %s" %S)
-A = Arestas(S)
-print("O número de arestas do grafo: %s" %A)
-Completo(S)
-Regular(S)
+def main():
+    arquivo = open("garfos.txt","r")
+    matriz = []
+    for linha in arquivo.readlines():
+        resultado = linha.strip().split()
+        matriz.append(resultado)
+    for linha in range(len(matriz)):
+        for coluna in range(len(matriz[linha])):
+             matriz[linha][coluna] = int(matriz[linha][coluna])
+
+    Printar(matriz)
+    grafoSimples(matriz)
+    S = Sequencia(matriz)
+    Arestas(S)
+    Completo(S)
+    Regular(S)
+    bipartido(matriz)
+
+main()
 
 
